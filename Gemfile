5c139e2b7dec636eb556a32dd1e6016017d5b5df
Alican Mutus <almutus@gmail.com>
2014-07-15 21:26:38 -0400

    devise integration beginning..

--- Gemfile
+++ Gemfile
@@ -19,7 +19,10 @@ gem 'bootstrap-sass', '~> 3.2.0'
 gem 'uglifier', '>= 1.3.0'
 
 # Auth
+
 gem 'devise'
+gem 'omniauth'
+gem 'omniauth-facebook'
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,18 @@ ProjectPHL::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Email
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => config.app_domain }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com', 
+    port: '587',
+    enable_starttls_auto: true,
+    user_name: 'someuser',
+    password: 'somepass',
+    authentication => :plain,
+    domain => 'somedomain.com'
+  }
 end
