37ffe03d9bc93ea4c0a8c5f01b2963df068c6561
Alican Mutus <almutus@gmail.com>
2014-07-15 21:43:23 -0400

    commented out email part

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,16 +31,16 @@ ProjectPHL::Application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   # Email
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => config.app_domain }
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com', 
-    port: '587',
-    enable_starttls_auto: true,
-    user_name: 'someuser',
-    password: 'somepass',
-    authentication => :plain,
-    domain => 'somedomain.com'
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.default_url_options = { :host => config.app_domain }
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com', 
+  #   port: '587',
+  #   enable_starttls_auto: true,
+  #   user_name: 'someuser',
+  #   password: 'somepass',
+  #   authentication => :plain,
+  #   domain => 'somedomain.com'
+  # }
 end
